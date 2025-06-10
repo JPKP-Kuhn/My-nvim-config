dacbb58ad174324c69e5765eb0483065758f6ec2
JPKP-Kuhn <pedrojoaojpk@gmail.com>
2025-06-10 10:53:14 -0300

    Adicionado variável INF

--- lua/templates/template.cpp
+++ lua/templates/template.cpp
@@ -5,7 +5,8 @@ using namespace std;
 using ll = long long;
 using vll = vector<ll>;
 
-const ll MAXN = pow(10, 5) + 10;
+const ll MAXN = pow(10, 5) + 10;// Range seguro para vetores e arrays
+const ll INF = 1e18+5;          // PD e inicialização de valor mínimo
 
 int main() {
   ios_base::sync_with_stdio(false);
