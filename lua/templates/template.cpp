eb36181c84d76d4796c94b34f1bd48d69536c511
JPKP-Kuhn <pedrojoaojpk@gmail.com>
2025-06-21 12:02:14 -0300

    Atualizando template

--- lua/templates/template.cpp
+++ lua/templates/template.cpp
@@ -5,8 +5,8 @@ using namespace std;
 using ll = long long;
 using vll = vector<ll>;
 
-const ll MAXN = pow(10, 5) + 10;// Range seguro para vetores e arrays
-const ll INF = 1e18+5;          // PD e inicialização de valor mínimo
+const ll MAXN = 1e5 + 10;// Range seguro para vetores e arrays
+const ll INF = 1e18+5;   // PD e inicialização de valor mínimo
 
 int main() {
   ios_base::sync_with_stdio(false);
