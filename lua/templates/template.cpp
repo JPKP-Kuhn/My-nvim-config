44c12c6fb6fdc7e82646159ee4a9f0f37ace4943
JPKP-Kuhn <pedrojoaojpk@gmail.com>
2025-07-26 16:03:18 -0300

    Improved template

--- lua/templates/template.cpp
+++ lua/templates/template.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
-#include <ios>
-#include <vector>
 using namespace std;
 using ll = long long;
 using vll = vector<ll>;
+using vb = vector<bool>;
+using vpll = vector<pair<ll, ll>>;
 
 const ll MAXN = 1e5 + 10;// Range seguro para vetores e arrays
 const ll INF = 1e18+5;   // PD e inicialização de valor mínimo
