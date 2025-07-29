8b0acc8482f29e8fbff221320bac483ee4e38d0b
JPKP-Kuhn <pedrojoaojpk@gmail.com>
2025-07-29 20:43:56 -0300

    Novos mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -11,3 +11,12 @@ map("i", "jk", "<ESC>")
 
 map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
+-- Somar 1 ao número sob o cursor
+map("n", "<C-=>", "<C-a>", { noremap = true, silent = true, desc = "Incrementar número (Ctrl + =)" })
+
+-- Subtrair 1 ao número sob o cursor
+map("n", "<C-->", "<C-x>", { noremap = true, silent = true, desc = "Decrementar número (Ctrl + -)" })
+
+-- Selecionar todo o texto com Ctrl + a
+map("n", "<C-a>", "ggVG", { noremap = true, silent = true, desc = "Selecionar tudo" })
+map("v", "<C-a>", "<Esc>ggVG", { noremap = true, silent = true, desc = "Selecionar tudo" })
