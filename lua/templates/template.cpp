d48510362f9819417ec173cefef2e2c2004a38ca
JPKP-Kuhn <pedrojoaojpk@gmail.com>
2025-09-26 16:30:39 -0300

    Update cpp template

--- lua/templates/template.cpp
+++ lua/templates/template.cpp
@@ -8,6 +8,7 @@ using vpll = vector<pair<ll, ll>>;
 
 const ll MAXN = 1e5 + 10;// Range seguro para vetores e arrays
 const ll INF = 1e18+5;   // PD e inicialização de valor mínimo
+const ll MOD = 1e9+7;    // Valor comum para aritmética modular, números muito grandes
 
 int main() {
   ios_base::sync_with_stdio(false);
